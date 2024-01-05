13358069667dc430f50c47cd7940bc7026f69b2a
Alejandro González <thytanium@gmail.com>
2024-01-05 17:55:58 -0300

    edit a to-do

--- src/components/todo-list/TodoList.tsx
+++ src/components/todo-list/TodoList.tsx
@@ -1,4 +1,3 @@
-import classNames from "classnames";
 import { CheckCircle, Circle } from "react-feather";
 import { Todo, TodoCreateCallback } from "../../models/Todo";
 import TodoCreate from "./TodoCreate";
@@ -19,27 +18,35 @@ export default function TodoList({
       <h1>To-do list</h1>
       <TodoCreate onTodoCreate={onTodoCreate} />
       {items.map((todo) => (
-        <article key={todo.id} className="flex items-baseline justify-between">
-          <div className="flex space-x-4 items-center">
+        <article key={todo.id} className="flex items-baseline">
+          <div className="flex-1 flex space-x-4 items-center">
             {todo.done ? (
               <CheckCircle className="text-green-500" />
             ) : (
               <Circle className="text-gray-200" />
             )}
-            <span
-              className={classNames("text-2xl font-light", {
-                "line-through": todo.done,
-              })}
-            >
-              {todo.text}
-            </span>
+            <div className="text-2xl font-light flex-1 px-1">
+              {todo.done ? (
+                <span className="line-through">{todo.text}</span>
+              ) : (
+                <input
+                  className="w-full"
+                  onChange={(event) =>
+                    onTodoUpdate({ ...todo, text: event.target.value })
+                  }
+                  value={todo.text}
+                />
+              )}
+            </div>
           </div>
-          <button
-            type="button"
-            onClick={() => onTodoUpdate({ ...todo, done: true })}
-          >
-            Done
-          </button>
+          {!todo.done && (
+            <button
+              type="button"
+              onClick={() => onTodoUpdate({ ...todo, done: true })}
+            >
+              Done
+            </button>
+          )}
         </article>
       ))}
     </section>
